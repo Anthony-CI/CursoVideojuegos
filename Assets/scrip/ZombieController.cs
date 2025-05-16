65197aa081a57e7bdb750791c5c33b48d8a990b1
Anthony <alci@gmail.com>
2025-05-16 08:54:26 -0500

    Semana8

---
+++ Assets/scrip/ZombieController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public int puntosvida = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
